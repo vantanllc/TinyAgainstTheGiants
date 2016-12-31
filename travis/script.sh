17db3a424c422b97ce1f11661aaa5fe735d50d91
Thinh Luong <vantan.llc@gmail.com>
2016-12-31 00:32:25 -0600

    Added travis and fastlane

---
+++ travis/script.sh
@@ -0,0 +1,9 @@
+if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+  open -b com.apple.iphonesimulator
+  fastlane ios test
+  fastlane ios report_test_coverage
+else
+  open -b com.apple.iphonesimulator
+  fastlane ios test
+  fastlane ios report_test_coverage
+fi
