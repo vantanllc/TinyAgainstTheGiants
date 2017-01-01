979664414a7e6bd7fb9cecd572068879b1beedb9
Thinh Luong <vantan.llc@gmail.com>
2017-01-01 09:53:14 -0600

    Added EntityBuilder

---
+++ TinyAgainstTheGiants/EntityBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  EntityBuilder.swift
+//  TinyAgainstTheGiants
+//
+//  Created by Thinh Luong on 1/1/17.
+//  Copyright © 2017 Vantan LLC. All rights reserved.
+//
+
+import GameplayKit
+
+class EntityBuilder {
+  // MARK: Functions
+  static func addPlayer(position: CGPoint, toEntityManager entityManager: EntityManager) {
+    let node = SKSpriteNode(color: .blue, size: CGSize(width: 40, height: 40))
+    let player = PlayerEntity(node: node)
+    entityManager.add(entity: player)
+  }
+}
---
+++ TinyAgainstTheGiantsTests/EntityBuilderTests.swift
@@ -0,0 +1,40 @@
+//
+//  EntityBuilderTests.swift
+//  TinyAgainstTheGiants
+//
+//  Created by Thinh Luong on 1/1/17.
+//  Copyright © 2017 Vantan LLC. All rights reserved.
+//
+
+import GameplayKit
+
+import Quick
+import Nimble
+@testable import TinyAgainstTheGiants
+
+class EntityBuilderSpec: QuickSpec {
+  override func spec() {
+    var entityManager: EntityManager!
+    var scene: SKScene!
+    
+    describe("EntityBuilder") {
+      beforeEach {
+        scene = SKScene()
+        entityManager = EntityManager(scene: scene)
+      }
+      
+      describe("addPlayer") {
+        it("should add PlayerEntity to entityManager") {
+          let position = CGPoint.zero
+          EntityBuilder.addPlayer(position: position, toEntityManager: entityManager)
+          
+          let playerEntity = entityManager.entities.filter { entity in
+            return entity.isKind(of: PlayerEntity.self)
+          }
+          
+          expect(playerEntity.first).toNot(beNil())
+        }
+      }
+    }
+  }
+}
