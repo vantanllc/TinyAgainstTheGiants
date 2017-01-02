735786292646b26b59459b43ed2e7ed1220875b7
Thinh Luong <vantan.llc@gmail.com>
2017-01-01 21:06:49 -0600

    Add support for creating TileMap with NoiseMap

--- TinyAgainstTheGiants/TileMapBuilder.swift
+++ TinyAgainstTheGiants/TileMapBuilder.swift
@@ -9,8 +9,33 @@
 import GameplayKit
 
 class TileMapBuilder {
-  static func createFilledTileMapWithTileSet(_ tileSet: SKTileSet, columns: Int, rows: Int) -> SKTileMapNode {
-    let tileMap = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSet.defaultTileSize, fillWith: tileSet.tileGroups.first!)
+  static func createFilledTileMapWithTileSet(_ tileSet: SKTileSet, columns: Int, rows: Int) -> SKTileMapNode? {
+    guard let tileGroup = tileSet.tileGroups.first else {
+      return nil
+    }
+    
+    let tileMap = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSet.defaultTileSize, fillWith: tileGroup)
+    tileMap.enableAutomapping = false
+    return tileMap
+  }
+  
+  static func createTileMapWithNoiseMap(_ noiseMap: GKNoiseMap, withTileSet tileSet: SKTileSet, columns: Int, rows: Int, threshold: Float = 0.5) -> SKTileMapNode? {
+    guard let tileGroup = tileSet.tileGroups.first else {
+      return nil
+    }
+    
+    let tileMap = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSet.defaultTileSize)
+    tileMap.enableAutomapping = true
+    
+    for column in 0..<columns {
+      for row in 0..<rows {
+        let vector = vector_int2(Int32(column), Int32(row))
+        if noiseMap.value(at: vector) >= threshold {
+          tileMap.setTileGroup(tileGroup, forColumn: column, row: row)
+        }
+      }
+    }
+    
     return tileMap
   }
 }
--- TinyAgainstTheGiantsTests/TileMapBuilderSpec.swift
+++ TinyAgainstTheGiantsTests/TileMapBuilderSpec.swift
@@ -14,27 +14,93 @@ import Nimble
 
 class TileMapBuilderSpec: QuickSpec {
   override func spec() {
-    describe("createFilledTileMapWithTileSet") {
-      let expectedColumns = 10
+    describe("TileMapBuilder") {
+      let expectedColumns = 9
       let expectedRows = 10
-      it("should return tilemap with expected column") {
-        let size = CGSize(width: 100, height: 100)
-        let texture = SKTexture(noiseWithSmoothness: 1.0, size: size, grayscale: true)
+      let expectedSize = CGSize(width: 100, height: 100)
+      var tileSet: SKTileSet!
+      var tileGroup: SKTileGroup!
+      var tileMap: SKTileMapNode!
+      
+      beforeEach {
+        let texture = SKTexture(noiseWithSmoothness: 1.0, size: expectedSize, grayscale: true)
         let tileDefinition = SKTileDefinition(texture: texture)
-        let tileGroup = SKTileGroup(tileDefinition: tileDefinition)
-        let tileSet = SKTileSet(tileGroups: [tileGroup])
-        let tileMap = TileMapBuilder.createFilledTileMapWithTileSet(tileSet, columns: expectedColumns, rows: expectedRows)
-        expect(tileMap.tileSize).to(equal(tileSet.defaultTileSize))
+        tileGroup = SKTileGroup(tileDefinition: tileDefinition)
       }
       
-      it("should return expected filled tilemap") {
-        let size = CGSize(width: 100, height: 100)
-        let texture = SKTexture(noiseWithSmoothness: 1.0, size: size, grayscale: true)
-        let tileDefinition = SKTileDefinition(texture: texture)
-        let tileGroup = SKTileGroup(tileDefinition: tileDefinition)
-        let tileSet = SKTileSet(tileGroups: [tileGroup])
-        let tileMap = TileMapBuilder.createFilledTileMapWithTileSet(tileSet, columns: expectedColumns, rows: expectedRows)
-        expect(tileMap.tileSize).to(equal(tileSet.defaultTileSize))
+      describe("createTileMapWithNoiseMap") {
+        
+        context("tileSet with tileGroup") {
+          beforeEach {
+            tileSet = SKTileSet(tileGroups: [tileGroup])
+            tileMap = TileMapBuilder.createTileMapWithNoiseMap(GKNoiseMap(), withTileSet: tileSet, columns: expectedColumns, rows: expectedRows)
+          }
+          
+          it("should return tilemap with automapping enabled") {
+            expect(tileMap.enableAutomapping).to(beTrue())
+          }
+          
+          it("should return tilemap with expected column") {
+            expect(tileMap.numberOfColumns).to(equal(expectedColumns))
+          }
+          
+          it("should return tilemap with expected row") {
+            expect(tileMap.numberOfRows).to(equal(expectedRows))
+          }
+          
+          it("should return tilemap with expected tileSize") {
+            expect(tileMap.tileSize).to(equal(expectedSize))
+          }
+        }
+        
+        context("tileSet without tileGroup") {
+          it("should return nil") {
+            tileSet = SKTileSet()
+            tileMap = TileMapBuilder.createTileMapWithNoiseMap(GKNoiseMap(), withTileSet: tileSet, columns: expectedColumns, rows: expectedRows)
+            expect(tileMap).to(beNil())
+          }
+        }
+      }
+      
+      describe("createFilledTileMapWithTileSet") {
+        context("tileSet without tileGroup") {
+          it("should return nil") {
+            tileSet = SKTileSet()
+            tileMap = TileMapBuilder.createFilledTileMapWithTileSet(tileSet, columns: expectedColumns, rows: expectedRows)
+            expect(tileMap).to(beNil())
+          }
+        }
+        
+        context("tileSet with tileGroup") {
+          beforeEach {
+            tileSet = SKTileSet(tileGroups: [tileGroup])
+            tileMap = TileMapBuilder.createFilledTileMapWithTileSet(tileSet, columns: expectedColumns, rows: expectedRows)
+          }
+          
+          it("should return tilemap with automapping disable") {
+            expect(tileMap.enableAutomapping).to(beFalse())
+          }
+          
+          it("should return tilemap with expected column") {
+            expect(tileMap.numberOfColumns).to(equal(expectedColumns))
+          }
+          
+          it("should return tilemap with expected row") {
+            expect(tileMap.numberOfRows).to(equal(expectedRows))
+          }
+          
+          it("should return tilemap with expected tileSize") {
+            expect(tileMap.tileSize).to(equal(expectedSize))
+          }
+          
+          it("should return tilemap filled with expected tileGroup") {
+            for column in 0..<tileMap.numberOfColumns {
+              for row in 0..<tileMap.numberOfRows {
+                expect(tileMap.tileGroup(atColumn: column, row: row)).to(equal(tileGroup))
+              }
+            }
+          }
+        }
       }
     }
   }
