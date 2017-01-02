4891560a85eab0c0b75fcc9ca9170d1dd509e6e5
Thinh Luong <vantan.llc@gmail.com>
2017-01-02 15:55:58 -0600

    Refactor game scene tests

--- TinyAgainstTheGiantsTests/GameSceneTests.swift
+++ TinyAgainstTheGiantsTests/GameSceneTests.swift
@@ -21,54 +21,47 @@ class GameSceneSpec: QuickSpec {
         gameScene = GameScene()
       }
       
-      describe("tilemap functions") {
-        describe("addBackgroundTileMap") {
-          it("should add currentBackgroundTileMap to scene as child") {
-            gameScene.addBackgroundTileMap()
-            expect(gameScene.currentBackgroundTileMap.scene).to(be(gameScene))
-          }
-        }
-        
-        describe("addObstacleTileMap") {
-          it("should add currentObstacleTileMap to scene as child") {
-            gameScene.addObstacleTileMap()
-            expect(gameScene.currentObstacleTileMap.scene).to(be(gameScene))
-          }
-        }
+      it("should initialize entityManager") {
+        expect(gameScene.entityManager).toNot(beNil())
       }
       
-      describe("startCamera") {
-        it("should set gameScene.camera") {
-          gameScene.startCamera()
-          expect(gameScene.camera).toNot(beNil())
-        }
+      it("should initialize worldNode") {
+        expect(gameScene.worldNode).toNot(beNil())
       }
       
-      describe("sceneDidLoad") {
-        it("should initialize entityManager") {
-          gameScene.sceneDidLoad()
-          expect(gameScene.entityManager).toNot(beNil())
-        }
-        
-        it("should initialize worldNode") {
-          gameScene.sceneDidLoad()
-          expect(gameScene.worldNode).toNot(beNil())
-        }
-        
-        it("should add worldNode to scene") {
-          gameScene.sceneDidLoad()
-          expect(gameScene.children).to(contain(gameScene.worldNode))
-        }
+      it("should add worldNode to scene") {
+        expect(gameScene.children).to(contain(gameScene.worldNode))
       }
       
-      describe("startNewGame") {
-        it("should add player's RenderComponent node to worldNode") {
-          gameScene.startNewGame()
-          if let expectedPlayerRenderNode = gameScene.entityManager.getPlayerRenderNode() {
-            expect(gameScene.worldNode.children).to(contain(expectedPlayerRenderNode))
-          } else {
-            fail("Unexpectedly got nil.")
-          }
+      it("should add currentBackgroundTileMap to scene as child") {
+        expect(gameScene.currentBackgroundTileMap.scene).to(be(gameScene))
+      }
+      
+      it("should add currentObstacleTileMap to scene as child") {
+        expect(gameScene.currentObstacleTileMap.scene).to(be(gameScene))
+      }
+      
+      it("should set gameScene.camera") {
+        expect(gameScene.camera).toNot(beNil())
+      }
+      
+      it("should initialize entityManager") {
+        expect(gameScene.entityManager).toNot(beNil())
+      }
+      
+      it("should initialize worldNode") {
+        expect(gameScene.worldNode).toNot(beNil())
+      }
+      
+      it("should add worldNode to scene") {
+        expect(gameScene.children).to(contain(gameScene.worldNode))
+      }
+      
+      it("should add player's RenderComponent node to worldNode") {
+        if let expectedPlayerRenderNode = gameScene.entityManager.getPlayerRenderNode() {
+          expect(gameScene.worldNode.children).to(contain(expectedPlayerRenderNode))
+        } else {
+          fail("Unexpectedly got nil.")
         }
       }
     }
