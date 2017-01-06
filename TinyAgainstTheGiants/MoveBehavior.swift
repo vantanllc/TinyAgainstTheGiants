d40e83557bdafc4d6309a8e540e2560fa3915723
Thinh Luong <vantan.llc@gmail.com>
2017-01-05 22:48:26 -0600

    Add MoveBehavior

---
+++ TinyAgainstTheGiants/MoveBehavior.swift
@@ -0,0 +1,30 @@
+//
+//  MoveBehavior.swift
+//  TinyAgainstTheGiants
+//
+//  Created by Thinh Luong on 1/5/17.
+//  Copyright © 2017 Vantan LLC. All rights reserved.
+//
+
+import GameplayKit
+
+class MoveBehavior: GKBehavior {
+  // MARK: Lifecycle
+  init(targetSpeed: Float, seek: GKAgent, avoid: [GKAgent]) {
+    goalToReachTargetSpeed = GKGoal(toReachTargetSpeed: targetSpeed)
+    goalToSeekAgent = GKGoal(toSeekAgent: seek)
+    goalToAvoidAgents = GKGoal(toAvoid: avoid, maxPredictionTime: maxPredictionTime)
+    super.init()
+    
+    setWeight(0.1, for: goalToReachTargetSpeed)
+    setWeight(0.5, for: goalToSeekAgent)
+    setWeight(0.3, for: goalToAvoidAgents)
+    
+  }
+  
+  // MARK: Properties
+  let goalToReachTargetSpeed: GKGoal!
+  let goalToSeekAgent: GKGoal!
+  let goalToAvoidAgents: GKGoal!
+  let maxPredictionTime: TimeInterval = 1
+}
---
+++ TinyAgainstTheGiantsTests/MoveBehaviorTests.swift
@@ -0,0 +1,49 @@
+//
+//  MoveBehaviorTests.swift
+//  TinyAgainstTheGiants
+//
+//  Created by Thinh Luong on 1/5/17.
+//  Copyright © 2017 Vantan LLC. All rights reserved.
+//
+
+import GameplayKit
+
+import Quick
+import Nimble
+@testable import TinyAgainstTheGiants
+
+class MoveBehaviorSpec: QuickSpec {
+  override func spec() {
+    describe("MoveBehavior") {
+      var behavior: MoveBehavior!
+      let expectedGoalCount = 3
+      let weightToReachTargetSpeed: Float = 0.1
+      let weightToSeekAgent: Float = 0.5
+      let weightToAvoidAgents: Float = 0.3
+      
+      let targetSpeed: Float = 200
+      let agent = GKAgent()
+      let agents = [GKAgent()]
+      
+      beforeEach {
+        behavior = MoveBehavior(targetSpeed: targetSpeed, seek: agent, avoid: agents)
+      }
+      
+      it("should have three goals") {
+        expect(behavior.goalCount).to(equal(expectedGoalCount))
+      }
+      
+      it("should have a goal toReachTargetSpeed with expected weight") {
+        expect(behavior.weight(for: behavior.goalToReachTargetSpeed)).to(equal(weightToReachTargetSpeed))
+      }
+      
+      it("should have a goal toSeekAgent with expected weight") {
+        expect(behavior.weight(for: behavior.goalToSeekAgent)).to(equal(weightToSeekAgent))
+      }
+      
+      it("should have a goal toAvoidAgents with expected weight") {
+        expect(behavior.weight(for: behavior.goalToAvoidAgents)).to(equal(weightToAvoidAgents))
+      }
+    }
+  }
+}
