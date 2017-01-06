21bb92e8f15718d5b3a75d4347d57ad135f22d5b
Thinh Luong <vantan.llc@gmail.com>
2017-01-05 23:06:39 -0600

    Add MoveComponent

---
+++ TinyAgainstTheGiants/MoveComponent.swift
@@ -0,0 +1,29 @@
+//
+//  MoveComponent.swift
+//  TinyAgainstTheGiants
+//
+//  Created by Thinh Luong on 1/5/17.
+//  Copyright © 2017 Vantan LLC. All rights reserved.
+//
+
+import GameplayKit
+
+class MoveComponent: GKAgent2D {
+  // MARK: Lifecycle
+  init(maxSpeed: Float, maxAcceleration: Float, radius: Float, mass: Float, entityManager: EntityManager) {
+    self.entityManager = entityManager
+    super.init()
+    
+    self.maxSpeed = maxSpeed
+    self.maxAcceleration = maxAcceleration
+    self.radius = radius
+    self.mass = mass
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: Properties
+  let entityManager: EntityManager
+}
---
+++ TinyAgainstTheGiantsTests/MoveComponentTests.swift
@@ -0,0 +1,50 @@
+//
+//  MoveComponentTests.swift
+//  TinyAgainstTheGiants
+//
+//  Created by Thinh Luong on 1/5/17.
+//  Copyright © 2017 Vantan LLC. All rights reserved.
+//
+
+import GameplayKit
+
+import Quick
+import Nimble
+@testable import TinyAgainstTheGiants
+
+class MoveComponentSpec: QuickSpec {
+  override func spec() {
+    var moveComponent: MoveComponent!
+    let maxSpeed: Float = 100
+    let maxAcceleration: Float = 200
+    let radius: Float = 50
+    let mass: Float = 10
+    let entityManager = EntityManager(scene: GameScene(size: CGSize.zero))
+    
+    describe("MoveComponent") {
+      beforeEach {
+        moveComponent = MoveComponent(maxSpeed: maxSpeed, maxAcceleration: maxAcceleration, radius: radius, mass: mass, entityManager: entityManager)
+      }
+      
+      it("should have an entityManager") {
+        expect(moveComponent.entityManager).to(be(entityManager))
+      }
+    
+      it("should set maxSpeed as specified") {
+        expect(moveComponent.maxSpeed).to(equal(maxSpeed))
+      }
+      
+      it("should set maxAcceleration as specified") {
+        expect(moveComponent.maxAcceleration).to(equal(maxAcceleration / mass))
+      }
+      
+      it("should set radius as specified") {
+        expect(moveComponent.radius).to(equal(radius))
+      }
+      
+      it("should set mass as specified") {
+        expect(moveComponent.mass).to(equal(mass))
+      }
+    }
+  }
+}
