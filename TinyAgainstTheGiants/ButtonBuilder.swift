b2c0d169e08bd2436050a10d5066ea6c9a0ad196
Thinh Luong <vantan.llc@gmail.com>
2017-01-16 20:28:20 -0600

    Add ability to build resume and pause button

--- TinyAgainstTheGiants/ButtonBuilder.swift
+++ TinyAgainstTheGiants/ButtonBuilder.swift
@@ -16,4 +16,20 @@ class ButtonBuilder {
     button.isUserInteractionEnabled = true
     return button
   }
+  
+  static func getResumeButton() -> ButtonNode {
+    let button = ButtonNode(imageNamed: "Resume")
+    button.zPosition = NodeLayerPosition.button
+    button.name = ButtonIdentifier.resume.rawValue
+    button.isUserInteractionEnabled = true
+    return button
+  }
+  
+  static func getPauseButton() -> ButtonNode {
+    let button = ButtonNode(imageNamed: "Pause")
+    button.zPosition = NodeLayerPosition.button
+    button.name = ButtonIdentifier.pause.rawValue
+    button.isUserInteractionEnabled = true
+    return button
+  }
 }
