162d09b4406d837155ad1eeb7e9b0ee547b7a329
Thinh Luong <vantan.llc@gmail.com>
2017-02-08 23:05:44 -0600

    Fix typos

--- travis/script.sh
+++ travis/script.sh
@@ -1,12 +1,14 @@
-if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_BRANCH = "production-release" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   open -b com.apple.iphonesimulator
   fastlane ios test
   fastlane ios report_test_coverage
+  fastlane ios build
+  fastlane ios deliver
+  fastlane ios beta
 else
   #open -b com.apple.iphonesimulator
   #fastlane ios test
   #fastlane ios report_test_coverage
-  #fastlane ios provision
   fastlane ios build
   fastlane ios deliver
   fastlane ios beta
