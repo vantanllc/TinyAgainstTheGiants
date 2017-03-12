35c0660adf8741d16754b6535f7e90b82d1d05dd
Thinh Luong <vantan.llc@gmail.com>
2017-03-11 22:57:37 -0600

    Refactor tests setup

--- TinyAgainstTheGiantsTests/GameScene+ButtonRespondableSpec.swift
+++ TinyAgainstTheGiantsTests/GameScene+ButtonRespondableSpec.swift
@@ -16,6 +16,7 @@ class GameSceneButtonRespondableSpec: QuickSpec {
   override func spec() {
     describe("GameScene+ButtonRespondable") {
       var gameScene: MockGameScene!
+      var button: ButtonNode!
       
       class MockGameScene: GameScene {
         override func showCredits() {
@@ -26,82 +27,59 @@ class GameSceneButtonRespondableSpec: QuickSpec {
       }
       
       beforeEach {
+        button = ButtonNode()
         gameScene = MockGameScene()
       }
       
       describe("buttonTrigger") {
         context("credits") {
-          var button: ButtonNode!
-          
-          beforeEach {
-            button = ButtonNode()
+          it("should show credits alertview") {
             button.name = ButtonIdentifier.credits.rawValue
             gameScene.buttonTriggered(button: button)
-          }
-          
-          it("should show credits alertview") {
             expect(gameScene.showCreditsWasCalled).to(beTrue())
           }
         }
         
         context("pause") {
-          var button: ButtonNode!
-          
-          beforeEach {
-            button = ButtonNode()
+          it("should enter PauseState") {
             button.name = ButtonIdentifier.pause.rawValue
             gameScene.buttonTriggered(button: button)
-          }
-          
-          it("should enter PauseState") {
             expect(gameScene.stateMachine.currentState).to(beAKindOf(GameScenePauseState.self))
           }
         }
         
         context("start") {
-          var button: ButtonNode!
-          
-          beforeEach {
-            button = ButtonNode()
+          it("should enter ActiveState") {
             button.name = ButtonIdentifier.start.rawValue
             gameScene.buttonTriggered(button: button)
-          }
-          
-          it("should enter ActiveState") {
             expect(gameScene.stateMachine.currentState).to(beAKindOf(GameSceneActiveState.self))
           }
         }
         
         context("resume") {
-          var button: ButtonNode!
-          
-          beforeEach {
-            button = ButtonNode()
+          it("should enter ActiveState") {
             button.name = ButtonIdentifier.resume.rawValue
             gameScene.stateMachine.enter(GameSceneFailState.self)
             gameScene.buttonTriggered(button: button)
-          }
-          
-          it("should enter ActiveState") {
             expect(gameScene.stateMachine.currentState).to(beAKindOf(GameSceneActiveState.self))
           }
         }
         
         context("retry") {
-          var button: ButtonNode!
-          
           beforeEach {
             for _ in 1...10 {
               gameScene.entityManager.add(entity: GKEntity())
             }
-            button = ButtonNode()
             button.name = ButtonIdentifier.retry.rawValue
             gameScene.stateMachine.enter(GameSceneFailState.self)
             gameScene.buttonTriggered(button: button)
           }
           
-          it("should remove all entities and add only the player entity") {
+          it("should remove all entities except for player") {
             expect(gameScene.entityManager.entities).to(haveCount(1))
+          }
+          
+          it("should add only the player entity") {
             expect(gameScene.entityManager.getPlayerEntity()).toNot(beNil())
           }
           
